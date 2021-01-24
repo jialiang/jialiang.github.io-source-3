fee873276ebced0b0d0f585f37294314ca424faf
Jia Liang <hojialiang@yahoo.com>
2021-01-25 07:12:41 +0800

    Add more responsive image sizes

--- src/images/generate-images.bat
+++ src/images/generate-images.bat
@@ -1,37 +1,61 @@
+call gm convert unused/project-1-1920.png -resize 1080x project-1-1080.webp
 call gm convert unused/project-1-1920.png -resize 780x project-1-780.webp
 call gm convert unused/project-1-1920.png -resize 520x project-1-520.webp
 call gm convert unused/project-1-1920.png -resize 360x project-1-360.webp
 
+call gm convert unused/project-1-1920.png -resize 1080x project-1-1080.jpg
 call gm convert unused/project-1-1920.png -resize 780x project-1-780.jpg
+call gm convert unused/project-1-1920.png -resize 520x project-1-520.jpg
+call gm convert unused/project-1-1920.png -resize 360x project-1-360.jpg
 
+call gm convert unused/project-2-1300.png -resize 1080x project-2-1080.webp
 call gm convert unused/project-2-1300.png -resize 780x project-2-780.webp
 call gm convert unused/project-2-1300.png -resize 520x project-2-520.webp
 call gm convert unused/project-2-1300.png -resize 360x project-2-360.webp
 
+call gm convert unused/project-2-1300.png -resize 1080x project-2-1080.jpg
 call gm convert unused/project-2-1300.png -resize 780x project-2-780.jpg
+call gm convert unused/project-2-1300.png -resize 520x project-2-520.jpg
+call gm convert unused/project-2-1300.png -resize 360x project-2-360.jpg
 
+call gm convert unused/project-3-1920.png -resize 1080x project-3-1080.webp
 call gm convert unused/project-3-1920.png -resize 780x project-3-780.webp
 call gm convert unused/project-3-1920.png -resize 520x project-3-520.webp
 call gm convert unused/project-3-1920.png -resize 360x project-3-360.webp
 
+call gm convert unused/project-3-1920.png -resize 1080x project-3-1080.jpg
 call gm convert unused/project-3-1920.png -resize 780x project-3-780.jpg
+call gm convert unused/project-3-1920.png -resize 520x project-3-520.jpg
+call gm convert unused/project-3-1920.png -resize 360x project-3-360.jpg
 
+call gm convert unused/project-4-1920.png -resize 1080x project-4-1080.webp
 call gm convert unused/project-4-1920.png -resize 780x project-4-780.webp
 call gm convert unused/project-4-1920.png -resize 520x project-4-520.webp
 call gm convert unused/project-4-1920.png -resize 360x project-4-360.webp
 
+call gm convert unused/project-4-1920.png -resize 1080x project-4-1080.jpg
 call gm convert unused/project-4-1920.png -resize 780x project-4-780.jpg
+call gm convert unused/project-4-1920.png -resize 520x project-4-520.jpg
+call gm convert unused/project-4-1920.png -resize 360x project-4-360.jpg
 
+call gm convert unused/project-5-1920.png -resize 1080x project-5-1080.webp
 call gm convert unused/project-5-1920.png -resize 780x project-5-780.webp
 call gm convert unused/project-5-1920.png -resize 520x project-5-520.webp
 call gm convert unused/project-5-1920.png -resize 360x project-5-360.webp
 
+call gm convert unused/project-5-1920.png -resize 1080x project-5-1080.jpg
 call gm convert unused/project-5-1920.png -resize 780x project-5-780.jpg
+call gm convert unused/project-5-1920.png -resize 520x project-5-520.jpg
+call gm convert unused/project-5-1920.png -resize 360x project-5-360.jpg
 
+call gm convert unused/project-6-1920.png -resize 1080x project-6-1080.webp
 call gm convert unused/project-6-1920.png -resize 780x project-6-780.webp
 call gm convert unused/project-6-1920.png -resize 520x project-6-520.webp
 call gm convert unused/project-6-1920.png -resize 360x project-6-360.webp
 
+call gm convert unused/project-6-1920.png -resize 1080x project-6-1080.jpg
 call gm convert unused/project-6-1920.png -resize 780x project-6-780.jpg
+call gm convert unused/project-6-1920.png -resize 520x project-6-520.jpg
+call gm convert unused/project-6-1920.png -resize 360x project-6-360.jpg
 
 pause
